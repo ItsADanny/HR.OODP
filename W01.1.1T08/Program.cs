25d916d2760f0d209a087c9898fa36667bc603ce
ItsDanny <danny.desnoo@ddesnoo.nl>
2024-09-03 11:45:29 +0200

    Commit 03/09/2024 11:45
    Finished nearly all the day 1 assignments for week 1.
    
    Only remaining assignment is the T9 assignment. Start has been made and has been put in this commit

--- W01.1.1T08/Program.cs
+++ W01.1.1T08/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿String str_choice;
+Int32 int32_pos_x = 0, int32_pos_y = 0;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("What direction would you like to go?");
+Int32 i = 0;
+while (i != 5) {
+    str_choice = Console.ReadLine().ToLower();
+
+    switch (str_choice)
+    {
+        case "up":
+            int32_pos_y++;
+            break;
+        case "down":
+            int32_pos_y--;
+            break;
+        case "right":
+            int32_pos_x++;
+            break;
+        case "left":
+            int32_pos_x--;
+            break;
+        default:
+            Console.WriteLine("Invalid direction");
+            break;
+    }
+
+    i++;
+}
+
+Console.WriteLine("Current position");
+Console.WriteLine("X:" + int32_pos_x + ", Y:" + int32_pos_y);
--- W01.1.1T09/Program.cs
+++ W01.1.1T09/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Int32 int32_age;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("");
+int32_age = Convert.ToInt32(Console.ReadLine());
+
+switch (int32_age)
+{
+    case int32_age >= 0 and <= 12:
+        Console.WriteLine("Age " + int32_age + ": a child");
+        break;
+    case int32_age is >= 13 and <= 19:
+        Console.WriteLine("Age " + int32_age + ": a teenager");
+        break;
+    case int32_age is >= 20 and <= 150:
+        Console.WriteLine("Age " + int32_age + ": an adult");
+        break;
+    default:
+        Console.WriteLine("Age " + int32_age + ": invalid");
+        break;
+}
