d75aedc8be96f9c1d5ed7da3ba926c042a0f9d8c
ItsDanny <141739582+ItsADanny@users.noreply.github.com>
2025-06-02 10:52:35 +0200

    Update 02/06/25
    Added comments

--- W15/Class Exercises/EnumSeasons/Program.cs
+++ W15/Class Exercises/EnumSeasons/Program.cs
@@ -42,23 +42,29 @@
 
     public static Season GetNextSeason(Season input)
     {
+        //Check to see if our maximum value is reached
         if (input == Season.Autumn)
         {
+            //If so return it to the first value (Winter)
             return Season.Winter;
         }
+        //If it isn't our maximum value, then increase our input Season and return it
         return input++;
     }
 
     public static void PrintAllSeasons()
     {
+        //Get all the values from our Enum Season
         foreach (var season in Enum.GetValues(typeof(Season)))
         {
+            //Print the value of our Enum per value
             Console.WriteLine($"({(int)season}) : {season}");
         }
     }
 
     public static string SeasonFeeling(Season input)
     {
+        //Switch expression that takes our inputted Season and returns a string based on its current value
         return input switch
         {
             Season.Winter => "Cold",
@@ -67,5 +73,20 @@
             Season.Autumn => "Mild",
             _ => "No valid data inputted"
         };
+
+        //Switch statement version
+        // switch (input)
+        // {
+        //     case Season.Winter:
+        //         return "Cold";
+        //     case Season.Spring:
+        //         return "Cool";
+        //     case Season.Summer:
+        //         return "Hot";
+        //     case Season.Autumn:
+        //         return "Mild";
+        //     default:
+        //         return "No valid data inputted";
+        // }
     }
 }
